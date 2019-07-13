5ed31949ef101e2da78f618dd921b51352f3d68c
Adrian <adrcichy@outlook.com>
2019-07-13 23:34:26 +0200

    uncomment try catch

--- flutter_learning/lib/domain/blocs/article_bloc.dart
+++ flutter_learning/lib/domain/blocs/article_bloc.dart
@@ -29,37 +29,38 @@ class ArticleBloc extends Bloc<ArticleEvent, ArticleState> {
   @override
   Stream<ArticleState> mapEventToState(ArticleEvent event) async* {
     if (event is FetchArticles && !_isMax(currentState)) {
-      // try {
-      if (currentState is ArticlesUninitialized) {
-        final articles = await _fetchArticles(1);
-        yield ArticlesFetched(
-            articles: articles,
-            currentPage: 1,
-            itemsNumber: articles.length,
-            isLastPage: false);
+      try {
+        if (currentState is ArticlesUninitialized) {
+          final articles = await _fetchArticles(1);
+          yield ArticlesFetched(
+              articles: articles,
+              currentPage: 1,
+              itemsNumber: articles.length,
+              isLastPage: false);
+        }
+
+        if (currentState is ArticlesFetched) {
+          final int currentPage = (currentState as ArticlesFetched).currentPage;
+
+          final articles = await _fetchArticles(currentPage + 1);
+
+          yield articles.isEmpty
+              ? (currentState as ArticlesFetched).copyState(isLpage: true)
+              : ArticlesFetched(
+                  articles:
+                      (currentState as ArticlesFetched).articles + articles,
+                  currentPage: currentPage + 1,
+                  itemsNumber: (currentPage + 1 * 20),
+                  isLastPage: false);
+        }
+      } catch (_) {
+        yield ArticleError();
       }
-
-      if (currentState is ArticlesFetched) {
-        final int currentPage = (currentState as ArticlesFetched).currentPage;
-
-        final articles = await _fetchArticles(currentPage + 1);
-
-        yield articles.isEmpty
-            ? (currentState as ArticlesFetched).copyState(isLpage: true)
-            : ArticlesFetched(
-                articles: (currentState as ArticlesFetched).articles + articles,
-                currentPage: currentPage + 1,
-                itemsNumber: (currentPage + 1 * 20),
-                isLastPage: false);
-      }
-      // } catch (_) {
-      //   yield ArticleError();
-      // }
     }
   }
 
-  bool _isMax(ArticleState state) => 
-    state is ArticlesFetched && state.isLastPage;
+  bool _isMax(ArticleState state) =>
+      state is ArticlesFetched && state.isLastPage;
 
   Future<List<Article>> _fetchArticles(int page) async {
     return await _articleRepository.getAllArticles(page);
