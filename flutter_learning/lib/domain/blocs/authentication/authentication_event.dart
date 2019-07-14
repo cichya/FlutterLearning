c40b5f11fb6f89f6bf16e50447a8ed3c639a4824
Adrian <adrcichy@outlook.com>
2019-07-14 19:22:23 +0200

    missing super added

--- flutter_learning/lib/domain/blocs/authentication/authentication_event.dart
+++ flutter_learning/lib/domain/blocs/authentication/authentication_event.dart
@@ -1,10 +1,22 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
-class AuthenticationEvent extends Equatable {}
+class AuthenticationEvent extends Equatable {
+  AuthenticationEvent([List properties = const[]]) : super(properties);
+}
+
+class AppStarted extends AuthenticationEvent {
+  @override
+  String toString() => 'AppStarted';
+}
 
 class LoggedIn extends AuthenticationEvent {
+  final String token;
+
+  LoggedIn({@required this.token}) : super([token]);
+
   @override
-  String toString() => 'LoggedIn';
+  String toString() => 'LoggedIn { token: $token }';
 }
 
 class LoggedOut extends AuthenticationEvent {
