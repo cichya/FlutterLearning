55800c5a1ba64225eca7abb9ddd0576e898b9a16
Adrian <adrcichy@outlook.com>
2019-07-14 18:04:53 +0200

    mock login repository added

---
+++ flutter_learning/lib/data/repositories/authentication_repository.dart
@@ -0,0 +1,7 @@
+import 'package:meta/meta.dart';
+
+class AuthenticationRepository {
+  Future<String> login({@required String userName, @required String password}) async {
+    return Future<String>.value('dwadkwad394399kwaodawoododood');
+  }
+}
