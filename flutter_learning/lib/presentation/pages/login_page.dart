9dd30e3493661e5fdeca2c457e81b2138def710b
Adrian <adrcichy@outlook.com>
2019-07-18 20:10:47 +0200

    simple inputs added

--- flutter_learning/lib/presentation/pages/login_page.dart
+++ flutter_learning/lib/presentation/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_learning/domain/blocs/login/login.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,13 +10,67 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Map<String, dynamic> _formData = {'username': null, 'password': null};
+
   @override
   Widget build(BuildContext context) {
+    final _loginBloc = BlocProvider.of<LoginBloc>(context);
+
+    _onLoginButtonPressed() {
+      _loginBloc.dispatch(Login(
+          userName: _formData['username'], password: _formData['password']));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Container(child: Text('Login kurła'),),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'User name',
+                      filled: true,
+                      fillColor: Colors.white),
+                  keyboardType: TextInputType.text,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a your user name';
+                    }
+                  },
+                  onSaved: (String value) {
+                    _formData['username'] = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white),
+                  obscureText: true,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                  },
+                  onSaved: (String value) {
+                    _formData['password'] = value;
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Login'),
+                  onPressed: () => _onLoginButtonPressed(),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
